8bdecd92757fd4ad674d17f1652b7b9742e582ef
Ana Lucia Rizzi Fiori <ana.fiori@inmetrics.com.br>
2022-08-24 15:53:24 -0300

    conversão de número romano para inteiro

---
+++ README.md
@@ -0,0 +1,4 @@
+Conversão de número romano para inteiro
+
+Com a utilização de um dicionário, este projeto recebe um número romano e devolve o número inteiro correspondente.
+
---
+++ Romanos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Romanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número romano:");
+            var numeroRomano = Console.ReadLine().ToUpper();
+
+            var dicionario = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+               
+            var numeroInt = 0;
+
+            try
+            {
+                for (var i = 0; i < numeroRomano.Length; i++)
+                {
+                    if (i < (numeroRomano.Length - 1) && dicionario[numeroRomano[i + 1]] > dicionario[numeroRomano[i]])
+                    {
+                        var numeroComposto = dicionario[numeroRomano[i + 1]] - dicionario[numeroRomano[i]];
+                        numeroInt += numeroComposto;
+                        i++;
+                    }
+                    else
+                    {
+                        numeroInt += dicionario[numeroRomano[i]];
+                    }
+                }
+
+                Console.WriteLine(numeroInt);
+            }
+            catch(KeyNotFoundException)
+            {
+                Console.WriteLine("Número inválido! Tente novamente!");
+            }
+        }
+    }
+}
